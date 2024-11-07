6933ffa77c7916a8ecd197740f84238928a2d5b8
ProximaSF <jaybird.walter@aol.com>
2024-11-07 15:03:39 -0500

    Change made by Jay

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-print("Jay was here ☺")
+import requests
+import json
+import random
+
+'''print("Jay was here ☺")
 print("Ismail says hi")
 print("Griffin says hey")
 print("Wuilmer was here.")
-print("John is late, but here")
+print("John is late, but here")'''
 
 # Function one
-def get_pokemon_info(pokemon_list):
+def get_pokemon_info(selected_pokemon, num_pokemon):
     try:
         with open("pokedex.json", "r", encoding="utf-8") as read_file:
             pokedex_data = json.load(read_file)
     except:
-        print("There was no json file called \"pokedex\" or file was empty, fixed")
+        print("There was no json file called \"pokedex\" or file was empty, fixing...")
         pokedex_data = {}
 
+    print("Please wait, gathering data...")
     def moves(moves_list):
         # print(moves_list)
-        num_moves = 10
+        num_moves = 4
         valid_moves_list = []
         for move_data in moves_list[:num_moves]:  # move_data is a dictionary
             move_name = move_data["move"]["name"]
@@ -23,37 +28,83 @@ def get_pokemon_info(pokemon_list):
             response = requests.get(move_url)
             if response.status_code == 200:
                 power_pokemon_data = response.json()
-                if power_pokemon_data["power"] == None:
-                    print(f"{move_name} deal zero damage")
-                else:
+                if power_pokemon_data["power"]:
                     add_move = {move_name: int(power_pokemon_data["power"])}
                     valid_moves_list.append(add_move)
+                else:
+                    #print(f"{move_name} deal zero damage")
+                    pass
         return valid_moves_list
 
-    for pokemon in pokemon_list:
-        if not pokedex_data.get(pokemon):
-            print(f"{pokemon} was not in json")
-            url = f"https://pokeapi.co/api/v2/pokemon/{pokemon.lower()}"
-            response = requests.get(url)
-            if response.status_code == 200:
-                pokemon_data = response.json()
+    def johto_pokemons():
+        pokemon_johto_generations_url = "https://pokeapi.co/api/v2/pokedex/3/"
+        response = requests.get(pokemon_johto_generations_url)
+        if response.status_code == 200:
+            johto_data = response.json()
+            johto_pokemon_list = [pokemon["pokemon_species"]["name"] for pokemon in johto_data["pokemon_entries"]]
+            #pokemon = random.choice(johto_pokemon_list)
+            #print(f"chose: {pokemon}")
+            return johto_pokemon_list
 
-                moves_list = pokemon_data["moves"] # list of data for each move
+    selected_pokedex_data = {}
+    def selected_pokemon_dict():
+        url = f"https://pokeapi.co/api/v2/pokemon/{selected_pokemon.lower()}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            pokemon_data = response.json()
 
-                pokedex_data[pokemon_data["name"]] = {
-                    "hp": int(pokemon_data["stats"][0]["base_stat"]),
-                    "basic_attack": int(pokemon_data["stats"][1]["base_stat"]),
-                    "defense": int(pokemon_data["stats"][2]["base_stat"]),
-                    "types": [types["type"]["name"] for types in pokemon_data["types"]],
-                    "moves": moves(moves_list)
-                }
+            moves_list = pokemon_data["moves"] # list of data for each move
+            selected_pokedex_data[pokemon_data["name"]] = {
+                "hp": int(pokemon_data["stats"][0]["base_stat"]),
+                "basic_attack": int(pokemon_data["stats"][1]["base_stat"]),
+                "defense": int(pokemon_data["stats"][2]["base_stat"]),
+                "types": [types["type"]["name"] for types in pokemon_data["types"]],
+                "moves": moves(moves_list)
+            }
+        return selected_pokedex_data
 
-                    # [type_data["type"]["name"] for type_data in pokemon_data["types"]]
-    with open("pokedex.json", 'w', encoding='utf-8') as write_file:
-        json.dump(pokedex_data, write_file, indent=2)
+    def pokedex_dict():
+        other_pokemons = {}
+        pokemon_names = johto_pokemons()
+        for i in range(num_pokemon):
+            picked_pokemon_name = random.choice(pokemon_names)
+            pokemon_names.remove(picked_pokemon_name)
+            if not pokedex_data.get(picked_pokemon_name):
+                #print(f"{picked_pokemon_name} was not in json")
+                url = f"https://pokeapi.co/api/v2/pokemon/{picked_pokemon_name.lower()}"
+                response = requests.get(url)
+                if response.status_code == 200:
+                    # print("Passed pokedex_dict response")
+                    pokemon_data = response.json()
 
-pokemons = ["pikachu", "bibarel"]
-get_pokemon_info(pokemons)
+                    moves_list = pokemon_data["moves"] # list of data for each move
+
+                    pokedex_data[pokemon_data["name"]] = {
+                        "hp": int(pokemon_data["stats"][0]["base_stat"]),
+                        "basic_attack": int(pokemon_data["stats"][1]["base_stat"]),
+                        "defense": int(pokemon_data["stats"][2]["base_stat"]),
+                        "types": [types["type"]["name"] for types in pokemon_data["types"]],
+                        "moves": moves(moves_list)
+                    }
+                    other_pokemons[picked_pokemon_name] = pokedex_data[picked_pokemon_name]
+            else:
+                other_pokemons[picked_pokemon_name] = pokedex_data[picked_pokemon_name]
+
+        with open("pokedex.json", 'w', encoding='utf-8') as write_file:
+            json.dump(pokedex_data, write_file, indent=2)
+        return other_pokemons
+
+    return selected_pokemon_dict(), pokedex_dict()
+
+selected_pokemon = "pikachu"
+num_pokemon = random.randrange(54, 180, step=18)
+print(f"{num_pokemon} different Pokemons will fight {selected_pokemon.capitalize()} one at a time")
+
+selected, others = get_pokemon_info(selected_pokemon, num_pokemon)
+print(selected)
+print("===================================================")
+print(others)
+
 
 # Function two
 
